e5d21ba221337bdb8a1f33dced96df541c6cd1e5
Armaan <armaansmhate@gmail.com>
2025-11-12 17:46:00 +0400

    temporarily changed CORS policy to check if app works with front end development env..

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ api = FastAPI()
 # Configure CORS
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Next.js dev server
+    allow_origins=["*"],  # Next.js dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
